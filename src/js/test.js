4a8ca2401fe7ea0f3eff2d33ee2b2067e9ba0d34
Alexander Frolov <big_air2409@mail.ru>
2019-06-03 17:24:55 +1200

    Added html webpack plugin

--- src/js/test.js
+++ src/js/test.js
@@ -1,2 +1,5 @@
 const ANSWER = 42;
+
+console.log("What is the answer to everything???");
+
 export default ANSWER;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
+// Plug ins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
-    }
-    // , mode: "development" // Uncomment or move to script in package.json
-    // , mode: "production"
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 }
 
 // Dev and production mode are set in npm script package.json
