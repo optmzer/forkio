aa80f9c5b280585eb9ce43e948d7eb0094614a39
Alexander Frolov <big_air2409@mail.ru>
2019-06-15 21:33:19 +1200

    Added file-loader, url-loader to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,48 +6,60 @@ const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    // target: 'node',
-    entry: ['babel-polyfill', './src/js/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist/'),
-        filename: 'js/bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new Dotenv(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+module.exports = env => {
+    return {
+        // target: 'node',
+        entry: ['babel-polyfill', './src/js/index.js'],
+        output: {
+            path: path.resolve(__dirname, 'dist/'),
+            filename: 'js/bundle.js'
+        },
+        devServer: {
+            contentBase: './dist'
+        },
+        plugins: [
+            new Dotenv(),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'style-loader',
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|jp(e*)g|svg)$/,
+                    use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8000,
+                            name: '[path][name].[ext]'
+                        }
+                    }]
                 }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    }
-}
+            ]
+        }
+    };
+};
 
 // Dev and production mode are set in npm script package.json
